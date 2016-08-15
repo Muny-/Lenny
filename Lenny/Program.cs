603c0322d520a985390c3ca26d8d1844d73add2a
Kevin <Kevin@KEVIN-DESKTOP>
2016-08-15 03:06:24 -0400

    Add project files.

---
+++ Lenny/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lenny
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            SendKeys.SendWait("{(} ͡° ͜ʖ ͡°{)}");
+            // hopefully that works?
+        }
+    }
+}
